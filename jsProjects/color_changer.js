4ea6be3d1f319a353a904dffea97ea324b67ce81
Nitin <nitinshankarmadhu@gmail.com>
2022-09-11 12:37:01 -0400

    hover effects now share color array generated

--- jsProjects/color_changer.js
+++ jsProjects/color_changer.js
@@ -4,13 +4,15 @@ let arr_gen_btn=document.querySelector("#arr_generate_button")
 let arr_t=document.querySelector("#arr_color_name")
 let root=document.querySelector(":root");
 
-nav_after=document.querySelector("nav ul::after");
-nav_before=document.querySelector("nav ul:before");
-
+nav1=document.querySelector("#st");
+nav2=document.querySelector("nd");
 
 root.style.setProperty('--default', 'lightblue');
 
 let randNo,randCode="";
+
+
+
 btn.addEventListener("click",function(){
     randCode="";
     for (let i=0;i<6;i++){
@@ -51,6 +53,7 @@ let arr_of_color=["#070707","40434E","C297B8","FDCFF3","DE89BE"];
 arr_gen_btn.addEventListener("click",function(){
     let choice=Math.floor(Math.random()*(arr_of_color.length));
     root.style.setProperty("--array","#"+arr_of_color[choice]);
-    arr_t.textContent=arr_of_color[choice];
-
+    arr_t.textContent="#" +arr_of_color[choice];
+    root.style.setProperty("--default","#"+arr_of_color[choice]);
 })
+
