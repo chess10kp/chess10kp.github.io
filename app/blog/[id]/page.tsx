f469daad7da5631df0700b9de4f54f8fbeff9300
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2024-11-29 15:45:17 -0500

    feat: add pagen not found for blog_ids

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -5,11 +5,25 @@ export default async function Page({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  const {content, metadata} = getPostById(id);
+  const { content, title, date } = getPostById(id);
   return (
     <div>
-      <h1>{id}</h1>
-      <h2 >{content}</h2>
-    </div> 
+      {title ? (
+        <div>
+          <h1>{title}</h1>
+          <h1>{date}</h1>
+          <p>{content}</p>
+        </div>
+      ) : (
+        <div className="text-center justify-center  min-h-[80vh] flex">
+          <div className="my-auto flex items-center">
+            <h1 className="border-r leading-loose align-top mx-8 px-8 border-r-foreground text-4xl">
+              404
+            </h1>
+            <p className="mx-2 font-light align-top">Page not found</p>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
