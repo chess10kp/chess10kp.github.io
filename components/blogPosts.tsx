e8956d6d857aedb12f4e9ec865016612835324ff
Nitin Shankar Madhu <nmadhu@umich.edu>
2026-05-29 20:42:09 -0400

    feat: updates

--- components/blogPosts.tsx
+++ components/blogPosts.tsx
@@ -24,40 +24,17 @@ const BlogPosts = ({ posts }: Props) => {
                 key={id}
                 animation="fade-up"
                 delay={index * 50 + 100}
-                className="flex items-start justify-between p-3 rounded-none bg-card/30 hover:bg-card/50  transition-all duration-300"
+                className="flex justify-center  p-3 rounded-none bg-card/30 hover:bg-card/50  transition-all duration-300"
               >
               <div className="flex-1">
-                <div className="flex items-center gap-2 items-center">
+                <div className="flex gap-2 items-center">
                 <Badge variant={"secondary"} className="text-xs font-mono">
                 {date}
                 </Badge>
-                  <Link href={`/blog/${id}`}>
-                    <h1 className="text-lg h-fit p-0 bg-white font-bold text-accent hover:text-accent/80 transition-colors font-mono">
+                  <Link className="inline-block max-h-fit p-0 m-0" href={`/blog/${id}`}>
                       {title}
-                    </h1>
                   </Link>
                   </div>
-                  {tagline && (
-                    <p className="text-sm text-muted-foreground/80 mt-1 line-clamp-2 font-mono">
-                      {tagline}
-                    </p>
-                  )}
-                </div>
-                <div className="flex flex-wrap gap-2 ml-4">
-                  {tags.slice(0, 3).map((tag) => (
-                    <Badge
-                      key={tag}
-                      variant="outline"
-                      className="transition-colors text-secondary text-xs font-mono"
-                    >
-                      {tag}
-                    </Badge>
-                  ))}
-                  {tags.length > 3 && (
-                    <Badge variant="secondary" className="text-xs font-mono">
-                      +{tags.length - 3}
-                    </Badge>
-                  )}
                 </div>
               </AnimatedSection>
             );
---
+++ posts/functional_programming_in_jac.md
@@ -0,0 +1,8 @@
+---
+title: Functional Programming in Jac
+date: "2026-04-24"
+tags: ["jac", "functional-programming"]
+---
+
+This post is a follow up to my [Functional Programming with C#](/blog/functional_programming_with_cs) post, that I wrote nearly a year ago 
+
